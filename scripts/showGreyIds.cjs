a088abd0605969a2785fe5743b528e8af99dbf10
Kjetil <kjetil@tilvest.net>
2026-03-18 07:38:37 +0100

    chore: remove debug scripts

--- scripts/showGreyIds.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const grey = JSON.parse(fs.readFileSync('data/greyEggData.json','utf8'));
-const ids = new Set([35,66]);
-console.log(grey.filter(e => ids.has(e.appearanceId)));
